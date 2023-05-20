1f14ee225f72f4b23875bef3f16f67eedfe6e477
Ilia Surikov <surikovis89@gmail.com>
2023-05-20 19:09:22 +0300

    Initial commit

---
+++ 5.7.1_Unique_vector/5.7.1_Unique_vector/5.7.1_Unique_vector.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	std::vector<int> v = { 1,1,2,5,6,1,2,4 };
+
+	std::cout << "[IN]:\t";
+	for (const auto &el : v)
+	{
+		std::cout << el << " ";
+	}
+
+	std::sort(v.begin(), v.end());
+	std::vector<int>::iterator it = std::unique(v.begin(), v.end());
+	v.erase(it, v.end());
+	
+	std::cout << "\n[OUT]:\t";
+	for (const auto &el : v)
+	{
+		std::cout << el << " ";
+	}
+}
---
+++ 5.7.2_Print_container_template/5.7.2_Print_container_template/5.7.2_Print_container_template.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+#include <vector>
+#include <list>
+
+template<typename T>
+void print_container(const T &container)
+{
+	for (const auto &el : container)
+	{
+		std::cout << el << " ";
+	}
+	std::cout << std::endl;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+
+	std::set<std::string> test_set = { "one", "two", "three", "four" };
+	print_container(test_set);
+
+	std::list<std::string> test_list = { "one", "two", "three", "four" };
+	print_container(test_list);
+
+	std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+	print_container(test_vector);
+}
